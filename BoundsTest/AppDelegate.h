3423ee97c7c1c0d70d3141888d0b1bb8723bc5d1
xionghx <xionghuanxin@huanxindemacbook-pro.local>
2016-07-13 10:30:20 +0800

    Initial Commit

---
+++ BoundsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoundsTest
+//
+//  Created by xionghuaxin on 7/13/16.
+//  Copyright © 2016 xionghuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoundsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoundsTest
+//
+//  Created by xionghuaxin on 7/13/16.
+//  Copyright © 2016 xionghuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
